9e38b64d0c407a3101d7beef3ea341fd6c258b1b
iriscrm <mnvx@yandex.ru>
2013-03-01 00:04:40 +0400

    ned

--- framework/project/view/site/terms.html.php
+++
@@ -1,4 +0,0 @@
-<?php namespace IrisPHPFramework; ?>
-  <h2><?php echo _('Terms of Service'); ?></h2>
-  
-  <p>These are our terms of service.</p>
--- framework/project/view/user/error.html.php
+++
@@ -1,5 +0,0 @@
-<?php namespace IrisPHPFramework; ?>
-
-	<h2>Authentication Error</h2>
-	
-	<p>You must be logged in or an administrator to access this content.  Please <a href="./user">login</a> and try again.</p>
